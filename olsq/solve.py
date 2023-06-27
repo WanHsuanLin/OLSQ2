a1b727fa2916340fbdaf7c8b218754eba00a8e91
WanHsuanLin <b06901054@ntu.edu.tw>
2023-06-27 09:59:21 -0700

    upd dump

--- olsq/solve.py
+++ olsq/solve.py
@@ -285,13 +285,17 @@ class OLSQ:
         self._add_injective_mapping_constraints(bound_depth, pi, lsqc)
         self._add_consistency_gate_constraints(bound_depth, pi, time, lsqc)
         self._add_dependency_constraints(lsqc, time, bound_depth)
-        self._add_swap_constraints(bound_depth, sigma, lsqc)
+        if self.mode == Mode.transition:
+                self._add_swap_constraints(bound_depth, sigma, lsqc)
+        else:
+            self._add_swap_constraints(bound_depth, sigma, lsqc, True, time, pi)
         self._add_transformation_constraints(bound_depth, lsqc, sigma, pi)
         length = int(math.log2(bound_depth))+1
         bit_tight_bound_depth = BitVecVal(bound_depth-1, length)
         lsqc.add([UGE(bit_tight_bound_depth, time[l]) for l in range(len(self.list_gate_qubits))])
         # print("time to generate constraints: {}".format(timeit.default_timer() - start))
-        # self._add_atmostk_cnf(lsqc, sigma, bound_swap, bound_depth-1)
+        if bound_swap >=0:
+            self._add_atmostk_cnf(lsqc, sigma, bound_swap, bound_depth-1)
         start = timeit.default_timer()
         tactic = Then('simplify','propagate-values','solve-eqs','card2bv','bit-blast', 'tseitin-cnf')
         output_file_name = folder+"/"+str(self.count_physical_qubit)+"_"+str(self.count_program_qubit) + "_" + str(bound_depth) + ".txt"
